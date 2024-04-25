05886f83dfb4d43fa701a1aa2df868ec807035f7
Luby94 <redi1024@naver.com>
2024-04-25 16:37:37 +0900

    axios03 추가 및 BusanBIMS data 불러오기 완료

--- src/main/java/com/green/controller/BusanBIMSController.java
+++ src/main/java/com/green/controller/BusanBIMSController.java
@@ -9,7 +9,10 @@ import java.net.URLEncoder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.green.dto.BusInfoDto;
 
 @Controller		// .jsp, .mustache 를 리턴 / .html 리턴하려면 @RestController 하거나 @ResponseBody
 public class BusanBIMSController {
@@ -17,23 +20,30 @@ public class BusanBIMSController {
 	@RequestMapping("/GetBus")
 	@ResponseBody
 	public String getBus(
-				
+			BusInfoDto dto
+	/*
+	 * @RequestParam("numOfRows") String numOfRows, String pageNo, String bstopnm,
+	 * String arsno
+	 */
 			) throws IOException {
+		
+		System.out.println( "===========================dto: " + dto );
 		
 		String serviceKey = "yxHUE0uF2KsGCO4QP2Oi4s9naMF16QnLfQbkMZpkref9Kc%2BbGl4XL5FETANVCilOu9GmkcS6sddUEndrXXTv3g%3D%3D";
 		
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6260000/BusanBIMS/busStopList"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode("부산시청", "UTF-8")); /*정류소 명*/
-        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode("13708", "UTF-8")); /*정류소 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(dto.getPageNo(), "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(dto.getNumOfRows(), "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode(dto.getBstopnm(), "UTF-8")); /*정류소 명*/
+        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode(dto.getArsno(), "UTF-8")); /*정류소 번호*/
+        //urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
         
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
+        System.out.println("===========================Response code: " + conn.getResponseCode());
         
         BufferedReader rd;
         if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
@@ -50,7 +60,7 @@ public class BusanBIMSController {
         
         rd.close();
         conn.disconnect();
-        System.out.println(sb.toString());
+        System.out.println("===========================sb.toString(): " + sb.toString());
 		
 		return sb.toString();
 		
---
+++ src/main/java/com/green/dto/BusInfoDto.java
@@ -0,0 +1,13 @@
+package com.green.dto;
+
+import lombok.Data;
+
+@Data
+public class BusInfoDto {
+
+	private String numOfRows;
+	private String pageNo;
+	private String bstopnm;
+	private String arsno;
+	
+}
